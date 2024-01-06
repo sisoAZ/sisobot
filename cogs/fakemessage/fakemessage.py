58ba6b7f91532ef61a9a0ee527a0c2098d39cd6b
sisoAZ <50191988+sisoAZ@users.noreply.github.com>
2024-01-06 23:02:54 +0900

    get display_name

--- cogs/fakemessage/fakemessage.py
+++ cogs/fakemessage/fakemessage.py
@@ -7,6 +7,7 @@ from cogs.fakemessage.discordmsg import discordMsg
 import discord
 from discord.ext import commands
 import os
+from util import async_get_json
 # Cog Beta-Release
 
 
@@ -25,20 +26,25 @@ class ImageDis(commands.Cog, name="こいつの発言捏造したろ！ｗ"):
         img_io.seek(0)
         # return img_io
 
-    @commands.command(name="fake", help="/fake [メッセージ]", description="他人の発言を捏造できます")
+    @commands.command(name="fake", help="/fake [メッセージ] \n 返信機能と組み合わせられます" , description="他人の発言を捏造できます")
     # @commands.guild_only()
-    async def discordMsgApi(self, ctx, *, text):
+    async def discordMsgApi(self, ctx, *, text=None):
         if ctx.message.reference != None:
             message = await ctx.message.channel.fetch_message(ctx.message.reference.message_id)
         else:
             message = ctx.message
         
         avatar_img = self._downloadExternalImg(message.author.avatar_url_as(size=128))
+        disaply_name_json = await async_get_json(f"https://dashboard.botghost.com/api/public/tools/user_lookup/{message.author.id}")
+        if disaply_name_json["global_name"] == None:
+            disaply_name = message.author.name
+        else:
+            disaply_name = disaply_name_json["global_name"]
         try:
             if text != None:
-                patternAppliedImg = await discordMsg(avatar_img, text, message.author.name, "#FFFFFF")
+                patternAppliedImg = await discordMsg(avatar_img, text, disaply_name, "#FFFFFF")
             else:
-                patternAppliedImg = await discordMsg(avatar_img, message.content, message.author.name, "#FFFFFF")
+                patternAppliedImg = await discordMsg(avatar_img, message.content, disaply_name, "#FFFFFF")
         except Exception as e:
             await ctx.send("Error")
         
@@ -52,4 +58,4 @@ class ImageDis(commands.Cog, name="こいつの発言捏造したろ！ｗ"):
 
 
 def setup(bot):
-    bot.add_cog(ImageDis(bot))
+    return bot.add_cog(ImageDis(bot))
